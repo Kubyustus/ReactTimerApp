63a951967eadda8048179f4af44049675aa61e7a
Kubyustus <neriad@gmail.com>
2017-10-31 13:49:15 +0200

    building timer and testing it

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,74 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
-var Timer = (props) => {
-  return (
-    <div>
-      <h1 className="text-center page-title">Timer</h1>
-    </div>
-  )
-};
+var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, PrevState) {
+    if (this.state.timerStatus !== PrevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  //componentWillUpdate: function (nextProps, nextState) {
+
+  //},
+  //componentWillMount: function () {
+  //  console.log('componentWillMount');
+  //},
+  //componentDidMount: function () {
+  //  console.log('componentDidMount');
+  //},
+  componentWillUnmount: function () {
+    //console.log('componentDidUnmount');
+    clearInterval(this.timer)
+    this.timer = undefined;
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      });
+    }, 1000);
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      timerStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({timerStatus: newStatus});
+  },
+  render: function () {
+    var {count, timerStatus} = this.state;
+    var renderControlArea = () => {
+      return <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>;
+    };
+
+    return (
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        {renderControlArea()}
+      </div>
+    );
+  }
+});
 
 module.exports = Timer;
